87e8c76873f69cd09df3c42c06330f44f2ac3008
Alex Kireev <a.kireev@g.nsu.ru>
2019-01-31 12:29:38 +0700

    intersection

--- CompTech/n-gramm.cpp
+++ CompTech/n-gramm.cpp
@@ -39,18 +39,41 @@ void NGramm_Spell_Checker::write_dictionary(ofstream& out, map<string, set<strin
 	}
 }
 
-std::vector<std::set<std::string>> intersect_sets(std::vector<std::set<std::string>> match_lists) {
+std::vector<std::set<std::string>> intersect_sets(std::vector<std::set<std::string>> match_lists, int shift) {
 	if (match_lists.size() == 1) return match_lists;
+	if (match_lists.size() - shift - 2 <= 0) return match_lists;
 	std::set<std::string> tmp;
 	std::set_intersection(
-		match_lists[match_lists.size() - 2].begin(), match_lists[match_lists.size() - 2].end(), 
-		match_lists[match_lists.size() - 1].begin(), match_lists[match_lists.size() - 1].end(),
+		match_lists[match_lists.size() - shift - 2].begin(), match_lists[match_lists.size() - shift - 2].end(), 
+		match_lists[match_lists.size() - shift - 1].begin(), match_lists[match_lists.size() - shift - 1].end(),
 		std::inserter(tmp, tmp.begin()));
-	if (tmp.empty()) return match_lists;
+	if (tmp.empty()) return intersect_sets(match_lists, shift + 1);
 	match_lists.pop_back();
 	match_lists.pop_back();
 	match_lists.push_back(tmp);
-	return intersect_sets(match_lists);
+	return intersect_sets(match_lists, shift);
+}
+
+std::set<std::string> intersect(std::vector<std::set<std::string>> match_lists) {
+	std::set<std::string> result;
+	std::set<std::string> buffer;
+
+	set_intersection(
+		match_lists[0].begin(), match_lists[0].end(),
+		match_lists[1].begin(), match_lists[1].end(),
+		std::inserter(result, result.begin()));
+
+	for (size_t i = 2; i < match_lists.size(); i++) {
+		buffer.clear();
+
+		set_intersection(
+			result.begin(), result.end(),
+			match_lists[i].begin(), match_lists[i].end(),
+			std::inserter(buffer, buffer.begin()));
+
+		swap(result, buffer);
+	}
+	return result;
 }
 
 std::multimap<size_t, std::string> NGramm_Spell_Checker::checkWord(const std::string & word) { // TODO: словарь сделать картой строк-карт
@@ -89,13 +112,11 @@ std::multimap<size_t, std::string> NGramm_Spell_Checker::checkWord(const std::st
 		//	
 		//}
 
-		std::vector<std::set<std::string>> result = intersect_sets(match_lists);
-		for (size_t i = 0; i < result.size(); i++) {
-			cout << i+1 << ") ------------------------" << endl;
-			for (auto j : result[i]) {
-				cout << j << endl;
-			}
+		std::set<std::string> result = intersect(match_lists);
+		for (auto i : result) {
+			cout << i << endl;
 		}
+
 		//cout << result.count("молоко") << endl;
 
 		/*for (auto i : match_lists) {
