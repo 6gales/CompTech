79b2ca2c1a104a0a4ec7d1bc4130b12518c0650f
YGAR84 <a.lyamin@g.nsu.ru>
2019-01-31 21:07:16 +0700

    some tests

--- CompTech/main.cpp
+++ CompTech/main.cpp
@@ -34,26 +34,20 @@ std::vector<std::string> correct, incorrect;
 
 void getExamples(const char* correct_source, const char* incorrect_source)
 {
-	//std::cout << correct_source << std::endl << incorrect_source << std::endl;
-	std::ifstream cor("C:/Users/YGAR/source/repos/CompTech3/CompTech/CompTech/correct.txt");
-	std::ifstream incor("C:/Users/YGAR/source/repos/CompTech3/CompTech/CompTech/incorrect.txt");
+	std::ifstream cor(correct_source);
+	std::ifstream incor(incorrect_source);
 
 	std::string buff;
-	//std::cout << "cor good " << cor.good() << std::endl;
-	//std::cout << cor.bad() << ' ' << cor.eof() << ' ' << cor.fail() << std::endl;
 	while (cor.good())
 	{
 		cor >> buff;
 		correct.push_back(buff);
 	}
-	//std::cout << "incor good " << incor.good() << std::endl;
-	//std::cout << incor.bad() << ' ' << incor.eof() << ' ' << incor.fail() << std::endl;
 	while (incor.good())
 	{
 		incor >> buff;
 		incorrect.push_back(buff);
 	}
-	//std::cout << correct.size() << ' ' << incorrect.size() << std::endl;
 	cor.close();
 	incor.close();
 	return;
@@ -66,30 +60,30 @@ int main(int argc, char** argv)
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
 
-	if (argc < 2) 
+	if (argc < 4) 
 	{
 		return 1;
 	}
-	//std::cout << "1.1" << std::endl;
 	std::string name(argv[1]);
 	std::ofstream output(getName(name) + "test.txt");
 	std::cout << "Uploading test files... ";
-	getExamples("correct.txt", "incorrect.txt");
+	getExamples(argv[3], argv[4]);
 	std::cout << "COMPLITED!" << std::endl;
 	bp::ipstream pipe_out;
 	bp::opstream pipe_in;
 	//std::cout << "1.2" << std::endl;
-	bp::child c(argv[1], bp::std_in < pipe_in, bp::std_out > pipe_out);
+	bp::child c(argv[1], argv[2], bp::std_in < pipe_in, bp::std_out > pipe_out);
+	//bp::child c(argv[1],)
 	//std::cout << "1.3" << std::endl;
 
 	std::string buffer;
 					
 	size_t begin = 0, 
 		end = 0,
-		step = 10000;
+		step = 100000;
 
-	std::cout << correct.size() << ' ' << incorrect.size() << std::endl;
-	std::cout << "1.4" << std::endl;
+	//std::cout << correct.size() << ' ' << incorrect.size() << std::endl;
+	//std::cout << "1.4" << std::endl;
 	while (true)
 	{
 		begin = rand() % correct.size();
@@ -101,45 +95,38 @@ int main(int argc, char** argv)
 		}
 	}
 
-	//std::cout << "1.5" << std::endl;
-	//std::cout << begin << ' ' << end << std::endl;
-	//std::cout << "1.6" << std::endl;
+	std::string answer = "not found";
+	size_t success = 0;
 	long long t1, t2;
 	t1 = clock();
 	for (size_t i = begin; i < end; ++i)
 	{
-		if ((i- begin) % 100 == 0) {	std::cout << "PROCESSING " << i - begin << std::endl;	}
+		if ((i - begin) % 1000 == 0) {	std::cout << "PROCESSING " << i - begin << std::endl;	}
 		bool wordFound = false;
-		std::vector<std::string> result;
 		pipe_in << incorrect[i] << std::endl;
 		size_t counter = 0;
 		while (pipe_out)
 		{
 			pipe_out >> buffer;
-			//std::cout << "buffer = " << buffer << std::endl;
 			if (buffer == "not found" || buffer == "end") { break; }
-			if (buffer == correct[i] && counter < 5) 
+			if (!wordFound && buffer == correct[i] && counter < 5)
 			{
+				answer = buffer;
 				wordFound = true;
+				++success;
 			}
 			++counter;
 		}
-		output << incorrect[i] << ';' << correct[i] << ';' << buffer << ';' <<  wordFound << std::endl;
+		output << incorrect[i] << ';' << correct[i] << ';' << answer << ';' <<  wordFound << std::endl;
+		answer = "not found";
 	}
 	pipe_in.pipe().close();
 	t2 = clock();
 	double average = ((double)(t2 - t1)) / ((double)step) / CLOCKS_PER_SEC;
-	output << "Average time on a word: " << average << " sec." << std::endl;
-	//pipe_in << "dadaya" << std::endl;
-	//pipe_in.pipe().close();
-
-	//std::string tmp;
-	//pipe_out >> tmp;
-	//std::cout << tmp << std::endl;
-
-	c.wait();
+	double chance = ((double)success) / ((double)step);
+	std::cout << success << ' ' << step << std::endl;
+	output << "Average time on a word: " << average << " sec." << std::endl << "chance = " << chance * 100 << '%' << std::endl;
+	
 	c.terminate();
-
-
 	return 0;
 }
