86b183f45244902cb6e92884d493c59cbee9b337
YGAR84 <a.lyamin@g.nsu.ru>
2019-01-31 15:40:47 +0700

    some changes

--- CompTech/main.cpp
+++ CompTech/main.cpp
@@ -34,26 +34,26 @@ std::vector<std::string> correct, incorrect;
 
 void getExamples(const char* correct_source, const char* incorrect_source)
 {
-	std::cout << correct_source << std::endl << incorrect_source << std::endl;
-	std::ifstream cor("input.txt");
-	std::ifstream incor("input.txt");
+	//std::cout << correct_source << std::endl << incorrect_source << std::endl;
+	std::ifstream cor("C:/Users/YGAR/source/repos/CompTech3/CompTech/CompTech/input.txt");
+	std::ifstream incor("C:/Users/YGAR/source/repos/CompTech3/CompTech/CompTech/incorrect.txt");
 
 	std::string buff;
-	std::cout << "cor good " << cor.good() << std::endl;
-	std::cout << cor.bad() << ' ' << cor.eof() << ' ' << cor.fail() << std::endl;
+	//std::cout << "cor good " << cor.good() << std::endl;
+	//std::cout << cor.bad() << ' ' << cor.eof() << ' ' << cor.fail() << std::endl;
 	while (cor.good())
 	{
 		cor >> buff;
 		correct.push_back(buff);
 	}
-	std::cout << "incor good " << incor.good() << std::endl;
-	std::cout << incor.bad() << ' ' << incor.eof() << ' ' << incor.fail() << std::endl;
+	//std::cout << "incor good " << incor.good() << std::endl;
+	//std::cout << incor.bad() << ' ' << incor.eof() << ' ' << incor.fail() << std::endl;
 	while (incor.good())
 	{
 		incor >> buff;
 		incorrect.push_back(buff);
 	}
-	std::cout << correct.size() << ' ' << incorrect.size() << std::endl;
+	//std::cout << correct.size() << ' ' << incorrect.size() << std::endl;
 	cor.close();
 	incor.close();
 	return;
@@ -70,22 +70,26 @@ int main(int argc, char** argv)
 	{
 		return 1;
 	}
-	std::cout << "1.1" << std::endl;
+	//std::cout << "1.1" << std::endl;
 	std::string name(argv[1]);
 	std::ofstream output(getName(name) + "test.txt");
+	std::cout << "Uploading test files... ";
 	getExamples("input.txt", "incorrect.txt");
+	std::cout << "COMPLITED!" << std::endl;
 	bp::ipstream pipe_out;
 	bp::opstream pipe_in;
-	std::cout << "1.2" << std::endl;
+	//std::cout << "1.2" << std::endl;
 	bp::child c(argv[1], bp::std_in < pipe_in, bp::std_out > pipe_out);
-	std::cout << "1.3" << std::endl;
+	//std::cout << "1.3" << std::endl;
 
 	std::string buffer;
-						
+					
+	size_t begin = 0, 
+		end = 0,
+		step = 10000;
 
-	int begin, end, step = 10000;
-	std::cout << correct.size() << ' ' << incorrect.size() << std::endl;
-	std::cout << "1.4" << std::endl;
+	//std::cout << correct.size() << ' ' << incorrect.size() << std::endl;
+	//std::cout << "1.4" << std::endl;
 	while (true)
 	{
 		begin = rand() % correct.size();
@@ -96,35 +100,38 @@ int main(int argc, char** argv)
 			break;
 		}
 	}
-	std::cout << "1.5" << std::endl;
-	std::cout << begin << ' ' << end << std::endl;
-	std::cout << "1.6" << std::endl;
+
+	//std::cout << "1.5" << std::endl;
+	//std::cout << begin << ' ' << end << std::endl;
+	//std::cout << "1.6" << std::endl;
 	long long t1, t2;
 	t1 = clock();
 	for (size_t i = begin; i < end; ++i)
 	{
+		//std::cout << "PROCESSING " << i - begin << std::endl;
 		bool wordFound = false;
 		std::vector<std::string> result;
-		pipe_in << ::incorrect[i] << std::endl;
+		pipe_in << incorrect[i] << std::endl;
 		while (pipe_out && getline(pipe_out, buffer))
 		{
 			if (buffer == "not found" || buffer == "end") {	break;	}
-			if (buffer == ::correct[i]) 
+			if (buffer == correct[i]) 
 			{
 				wordFound = true;
 			}
 		}
-		output << ::incorrect[i] << ';' << ::correct[i] << ';' << wordFound << std::endl;
+		output << ::incorrect[i] << ';' << ::correct[i] << ';' << buffer << ';' <<  wordFound << std::endl;
 	}
+	pipe_in.pipe().close();
 	t2 = clock();
 	double average = ((double)(t2 - t1)) / ((double)step) / CLOCKS_PER_SEC;
 	output << "Average time on a word: " << average << " sec." << std::endl;
 	//pipe_in << "dadaya" << std::endl;
 	//pipe_in.pipe().close();
 
-	std::string tmp;
-	pipe_out >> tmp;
-	std::cout << tmp << std::endl;
+	//std::string tmp;
+	//pipe_out >> tmp;
+	//std::cout << tmp << std::endl;
 
 	c.wait();
 	c.terminate();
