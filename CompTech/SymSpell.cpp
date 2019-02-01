4d885407f6817020880defb415249f1f1b127d9b
6gales <6gales@gmail.com>
2019-02-01 14:46:09 +0700

    Updated symspell

--- CompTech/SymSpell.cpp
+++ CompTech/SymSpell.cpp
@@ -2,33 +2,21 @@
 
 void SymSpell::fillDeletionsMap(const std::string &word, size_t pos)
 {
-	editDistance = 2; //editDistFormula(word.size());
-	
-	std::map <std::string, size_t> toInsert;
-	generateDeletions(word, editDistance, toInsert);
-	
-	for (auto it : toInsert)
-		generated.insert({ it.first, { it.second, pos } });
-}
+	editDistance = editDistFormula(word.size());
 
-void SymSpell::generateDeletions(const std::string &word,
-	size_t distance, std::map <std::string, size_t> &deletions)
-{
-	deletions.insert({ word, editDistance - distance });
-	if (!distance || word.size() <= 1)
-		return;
-
-	std::string wDeletion;
-	for (size_t i = 0; i < word.size(); i++)
-	{
-		wDeletion = word;
-		wDeletion.erase(wDeletion.begin() + i);
-		generateDeletions(wDeletion, distance - 1, deletions);
-	}
+	generateDeletions(word, editDistance,
+		[this](const std::string &word, size_t dist, size_t pos) mutable
+		{
+			auto inserted = generated.insert({ word, {} });
+			inserted.first->second.emplace_front(dist, pos);
+		}, pos);
 }
 
 SymSpell::SymSpell(const char *name) : SpellChecker(name)
 {
+	editDistFormula = [](size_t sz) -> size_t
+	{ size_t ret = sz / 2 + sz % 2; if (ret > 2) return 2; return ret; };
+
 	std::string word;
 	while (dictionary.good())
 	{
@@ -40,25 +28,32 @@ SymSpell::SymSpell(const char *name) : SpellChecker(name)
 
 std::multimap<size_t, std::string> SymSpell::checkWord(const std::string &word)
 {
-	editDistance = 2; //editDistFormula(word.size());
+	editDistance = editDistFormula(word.size());
 	
 	std::map <std::string, size_t> deletions;
-	generateDeletions(word, editDistance, deletions);
+	generateDeletions(word, editDistance,
+		[&deletions](const std::string &word, size_t distance) mutable
+		{
+			deletions.insert({ word, distance });
+		});
 
 	std::map <std::string, size_t> reverseSuggestions;
-	for (auto i : deletions)
+	for (auto del : deletions)
 	{
-		for (auto fnd = generated.find(i.first); fnd != generated.end(); ++fnd)
+		auto fnd = generated.find(del.first);
+		
+		if (fnd == generated.end())
+			continue;
+		
+		for (auto it = fnd->second.begin(); it != fnd->second.end(); ++it)
 		{
-			if (fnd->first != i.first)
-				break;
-
-			if (fnd->second.first <= editDistance)
+			if (it->first <= editDistance)
 			{
-				size_t priority = (fnd->second.first > i.second ? fnd->second.first : i.second);
-				auto it = reverseSuggestions.insert({ originals[fnd->second.second], priority });
-				if (priority < it.first->second)
-					it.first->second = priority;
+				size_t priority = (it->first > del.second ? it->first : del.second);
+				auto insrtRes = reverseSuggestions.emplace(originals[it->second], priority);
+					//.insert({ originals[fnd->second[i].second], priority });
+				if (priority < insrtRes.first->second)
+					insrtRes.first->second = priority;
 			}
 		}
 	}
--- CompTech/SymSpell.h
+++ CompTech/SymSpell.h
@@ -1,13 +1,15 @@
 #pragma once
+#include <forward_list>
 #include <vector>
 #include "Interface.h"
 
 class SymSpell : public SpellChecker
 {
 	std::vector <std::string> originals;
-	std::multimap <std::string, std::pair <size_t, size_t>> generated;
+	std::map <std::string, std::forward_list <std::pair <size_t, size_t>>> generated;
 
-	void generateDeletions(const std::string &, size_t, std::map <std::string, size_t> &);
+	template <class function, class ... args>
+	void generateDeletions(const std::string &, size_t, function, args...);
 
 	void fillDeletionsMap(const std::string &, size_t);
 
@@ -16,3 +18,21 @@ public:
 
 	std::multimap <size_t, std::string> checkWord(const std::string &);
 };
+
+template <class function, class ... args>
+void SymSpell::generateDeletions(const std::string &word,
+	size_t distance, function foo, args ... parameters)
+{
+	foo(word, editDistance - distance, parameters...);
+
+	if (!distance || word.size() <= 1)
+		return;
+
+	std::string wDeletion;
+	for (size_t i = 0; i < word.size(); i++)
+	{
+		wDeletion = word;
+		wDeletion.erase(wDeletion.begin() + i);
+		generateDeletions(wDeletion, distance - 1, foo, parameters...);
+	}
+}
